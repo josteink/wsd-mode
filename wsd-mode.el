d83e5c70004830d02a7361434ecee5e2af7a6fc5
Jostein Kjønigsen <jostein@kjonigsen.net>
2014-12-04 22:08:35 +0100

    Preliminary research-work on syntax-highlighting in major-modes.

--- wsd-mode.el
+++ wsd-mode.el
@@ -1,6 +1,26 @@
 
 (require 'wsd-core)
 
-;; TODO: create major-mode.
+;; notes about derived mode here: http://www.emacswiki.org/emacs/DerivedMode
 
-(global-set-key (kbd "C-<f5>") 'wsd-process)
+(defface wsd-face-1 
+  '((t (:foreground "#0000FF")))
+  "blue")
+
+(setq wsd-keyswords '("title"
+		      "participant" "as"
+		      "-->-" "-->" "->+" "->-" "->"
+		      "alt" "else" "end"
+		      "note" "over" "right" "left" "of"))
+
+;; map keywords to font-faces
+(setq wsd-keyword-list
+      (mapcar (lambda (i)
+		(list (regexp-quote i) 'quote 'wsd-face-1))
+	      wsd-keyswords))
+
+(define-derived-mode wsd-mode fundamental-mode "wsd-mode"
+  "Major-mode for websequencediagrams.com"
+  (font-lock-add-keywords nil wsd-keyword-list)
+
+  (local-set-key (kbd "C-c C-c") 'wsd-process))
