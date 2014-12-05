a8c2791b961f1101d0cb6cd64940ff8e15e5c712
Jostein Kjønigsen <jostein@kjonigsen.net>
2014-12-05 14:16:29 +0100

    Fix tests.

--- wsd-tests.el
+++ wsd-tests.el
@@ -7,16 +7,19 @@
   (string-suffix-p)
   )
 
-(setq file-names '(nil, "lalalala", "test.txt", "test.wsd", "test.a.wsd"))
+(setq file-names '(nil "lalalala" "test.txt" "test.wsd" "test.a.wsd"))
 
 (ert-deftest local-filename-is-never-nil ()
   (dolist (file-name file-names)
     (let* ((image-name (wsd-get-image-filename file-name)))
       (assert
-       (not (equal (nil image-name)))))))
+       (not (equal nil image-name))))))
 
-(ert-deftest local-filename-is-never-nil ()
+(ert-deftest local-filename-always-contains-extension ()
   (dolist (file-name file-names)
     (let* ((image-name (wsd-get-image-filename file-name)))
       (assert
        (string-suffix-p wsd-format image-name)))))
+
+;;(ert-run-tests-interactively t)
+
