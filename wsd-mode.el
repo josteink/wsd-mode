4a7f08cae0b653ffddd1b5fde33098f20dc1b833
Jostein Kjønigsen <jostein@kjonigsen.net>
2014-12-05 23:08:20 +0100

    First real working major-mode with fontification.
    Closes https://github.com/josteink/wsd-mode/issues/1.

--- wsd-mode.el
+++ wsd-mode.el
@@ -1,31 +1,37 @@
+;;; wsd-mode.el --- Emacs major-mode for www.websequencediagrams.com
 
-;; Version : 0.0.1
+;; Author     : Jostein Kjønigsen <jostein@gmail.com>
+;; Created    : December 2014
+;; Modified   : November 2014
+;; Version    : 0.0.1
+;; Keywords   : wsd diagrams design process modelling
+;; X-URL      : https://github.com/josteink/wsd-mode
+;; 
 
 (require 'wsd-core)
 
 ;; notes about derived mode here: http://www.emacswiki.org/emacs/DerivedMode
 
-(defface wsd-face-1 
-  '((t (:foreground "#0000FF")))
-  "blue")
+(defun wsd-create-font-lock-list (face list)
+  (mapcar (lambda (i)
+            (list (regexp-quote i) 'quote face))
+          list))
 
-(setq wsd-keyswords '("title"
-		      "participant" "as"
-		      "-->-" "-->" "->+" "->-" "->"
-		      "alt" "else" "end"
-		      "note" "over" "right" "left" "of"))
-
-;; map keywords to font-faces
-(setq wsd-keyword-list
-      (mapcar (lambda (i)
-		(list (regexp-quote i) 'quote 'wsd-face-1))
-	      wsd-keyswords))
+(defun wsd-add-keywords (face list)
+  (let* ((klist (wsd-create-font-lock-list face list)))
+    (font-lock-add-keywords nil klist)))
 
 (define-derived-mode wsd-mode fundamental-mode "wsd-mode"
   "Major-mode for websequencediagrams.com"
-  (font-lock-add-keywords nil wsd-keyword-list)
+  (wsd-add-keywords 'font-lock-keyword-face
+                    '("title"
+                      "participant" " as "
+
+                      "alt" "else" "end"
+                      "note" "over" "right" "left" "of"))
+  (wsd-add-keywords 'font-lock-comment-face
+		    '("-->-" "-->" "->+" "->-" "->" ": "))
 
   (local-set-key (kbd "C-c C-c") 'wsd-process))
-
 
 (provide 'wsd-mode)
