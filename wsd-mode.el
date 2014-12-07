0a9fd975fde2c402e426d2c0d52fd4e5bcfcb0be
Jostein Kjønigsen <jostein@kjonigsen.net>
2014-12-07 20:15:43 +0100

    Preliminary work on indentation.
    Addresses issue https://github.com/josteink/wsd-mode/issues/3

--- wsd-mode.el
+++ wsd-mode.el
@@ -21,6 +21,36 @@
   (let* ((klist (wsd-create-font-lock-list face list)))
     (font-lock-add-keywords nil klist)))
 
+(defvar wsd-indent-offset 4
+  "*Indentation offset for `wsd-mode'.")
+
+(defun wsd-indent-line ()
+  "Indent current line for `wsd-mode'."
+  (interactive)
+  (let ((indent-col 0)
+        (indent-mappings (list
+                          (cons "alt " '+)
+                          (cons "opt " '+)
+                          (cons "loop" '+)
+                          (cons "else" '+)
+                          (cons "else" '-)
+			  (cons "end" '-))))
+    (save-excursion
+      (beginning-of-line)
+      (condition-case nil
+          (while t
+            (previous-line)
+            (beginning-of-line-text)
+            (dolist (mapping indent-mappings)
+              (let* ((keyword (car mapping))
+                     (func    (cdr mapping)))
+                (when (looking-at keyword)
+                  (setq indent-col (funcall func indent-col wsd-indent-offset)))))
+)
+        (error nil)))
+    (setq indent-col (max 0 indent-col))
+    (indent-line-to indent-col)))
+
 ;;;###autoload
 (define-derived-mode wsd-mode fundamental-mode "wsd-mode"
   "Major-mode for websequencediagrams.com"
@@ -33,13 +63,16 @@
 
   ;; lines starting with # are actual commens
   (let* ((operator-list '("-->-" "-->" "->+" "->-" "->" ": " "#"))
-	 (operators      (wsd-create-font-lock-list 'font-lock-comment-face operator-list)))
+         (operators      (wsd-create-font-lock-list 'font-lock-comment-face operator-list)))
     ;;(setq operators (cons (cons (regexp-quote "#") 'font-lock-command-face) operators))
     (font-lock-add-keywords nil operators))
 
   (local-set-key (kbd "C-c C-c") 'wsd-show-diagram-inline)
   (local-set-key (kbd "C-c C-e") 'wsd-show-diagram-online)
-  (local-set-key (kbd "C-c C-k") 'wsd-strip-errors))
+  (local-set-key (kbd "C-c C-k") 'wsd-strip-errors)
+
+  (make-local-variable 'wsd-indent-offset)
+  (set (make-local-variable 'indent-line-function) 'wsd-indent-line))
 
 ;;; Autoload mode trigger
 ;;;###autoload
